97a364c405d11d470ebf9c1ac900ee3c41fcde37
OksanaKuzich <komoks255@gmail.com>
2022-11-22 18:55:58 +0200

    fixed trending movies

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,13 @@
 import { fetchTrendingMovies } from 'service/api';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { TrendingList } from 'components/TrendingList/TrendingList';
 import { Section } from './Home.styles';
 
 export const Home = () => {
   const [gallery, setGallery] = useState([]);
-  const isFirstRender = useRef(true);
   const pathLocation = '/movies/';
 
   useEffect(() => {
-    if (isFirstRender.current) {
-      isFirstRender.current = false;
-      return;
-    }
 
     fetchTrendingMovies()
       .then(setGallery)
